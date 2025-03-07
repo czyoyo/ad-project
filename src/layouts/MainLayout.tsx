b76ada4c45c55efaf5aa537b88dbe12e4b5241d3
home <cz2123zc@gmail.com>
2025-03-07 19:39:37 +0900

    css 주석

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -25,7 +25,7 @@ function MainLayout(): JSX.Element {
             <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500"></div>
           </div>
         ) : (
-          <Outlet />
+          <Outlet /> // 하위 라우트를 렌더링
         )}
       </main>
 
